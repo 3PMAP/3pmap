d11f751ab7f241b6b4ffa1ec3a8619976bb2626b
Yochem <6283@pierson.nl>
2017-09-21 21:58:22 +0200

    Updated core, first code

--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,4 @@
-const int switchPin = 2;
+int switchPin = 2;
 
 // Setup for motorpins
 const int motor1 = 9;
@@ -9,16 +9,13 @@ const int motor5 = 13;
 
 int switchState = 0;
 
-int motors[] = {
-  9, 10, 11, 12, 13
-};
-
-
 // setup code
 void setup() {
-  for (int m = 0; m < 5; m++) {
-    pinMode(motors[m], OUTPUT);
-  }
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+  pinMode(motor3, OUTPUT);
+  pinMode(motor4, OUTPUT);
+  pinMode(motor5, OUTPUT);
 
   pinMode(switchPin, INPUT);
 }
@@ -26,26 +23,27 @@ void setup() {
 
 
 
-// re-usable functions
-stopAllMotors() {
-  digitalWrite(motorPin, LOW);
-}
-// or
-stopAllMotos() {
-  for (int m = 0; m < 5; m++) {
-    pinMode(motors[m], LOW);
-  }
+// call functions
+void stopAllMotors() {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, LOW);
+  digitalWrite(motor4, LOW);
+  digitalWrite(motor5, LOW);
 }
 
-
-
-
-
+void startAllMotors() {
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, HIGH);
+  digitalWrite(motor5, HIGH);
+}
 
 // main code
 void loop() {
   switchState = digitalRead(switchPin);
 
-  digitalWrite(motorPin, switchState);
-  delay(1000)
+  digitalWrite(motor1, switchState);
+  delay(1000);
 }
